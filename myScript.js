b2e589a0d5e5d90786401f4a5779ea08efb1ef88
Matt602 <matthew.frierson062@gmail.com>
2022-08-09 01:40:53 -0500

    Created a function to get player choice

--- myScript.js
+++ myScript.js
@@ -1,7 +1,14 @@
 function getComputerChoice()
 {
     let choice = ['Rock', 'Paper', 'Scissors'];
-    return choice[Math.floor(Math.random() * 3)]
+    return choice[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice()
+{
+    let text = prompt('Enter rock, paper, or scissors: ');
+    let choice = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+    return choice;
+}
+
+console.log(getPlayerChoice());
